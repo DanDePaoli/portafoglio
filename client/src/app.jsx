6e79a039e62f71a22cac7a6ad24ab56f5cce1c9c
Daniel DePaoli <dannydepaoli0@gmail.com>
2020-12-08 12:26:51 -0800

    Package JSON configuration

---
+++ client/src/app.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import axios from 'axios';
+
+
+class App extends React.Component {
+constructor(props){
+super(props);
+this.state = {}
+
+}
+
+render () {
+  return (
+<div>
+
+React is working!
+
+</div>
+  );
+
+}
+
+
+}
+
+export default App;

ae906911886c012caf54c68ea8ed30f8d7c8f86e
Daniel DePaoli <dannydepaoli0@gmail.com>
2020-12-10 10:35:48 -0800

    Skill chips and icons

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { GrEmptyCircle } from "react-icons/gr";
+import { Chip } from 'react-mdl';
+import { DiReact, DiJavascript1, DiNodejsSmall, DiHtml5, DiMysql, DiMongodb, DiPostgresql } from "react-icons/di";
 
 var Resume = (props) => (
 
@@ -9,10 +10,17 @@ var Resume = (props) => (
 
 <h1 className="AboutMe-ThanksForStoppingBy">Resume</h1>
 <div className="Resume-CareerTimeline">
-  <div className = "Resume-Trail"/>
-  <div className = "Resume-Icon"/>
 
-{/* <GrEmptyCircle className = "Resume-Icon"/> */}
+<p className="Resume-OrgName">Work Experience</p>
+
+
+  <div className="Resume-Trail"/>
+  <div className="Resume-Icon"/>
+  <div className="Resume-OrgDiv">
+  <p className="Resume-OrgName">Contract Software Engineer</p>
+  <p className="Resume-OrgName">Clients: Oli Gallery, Banter</p>
+  <div className="Resume-SkillChip"><Chip><DiJavascript1/> Javascript</Chip>   <Chip><DiNodejsSmall/> Node.js</Chip> <Chip><DiReact/> React.js</Chip> <Chip><DiHtml5/> HTML/CSS</Chip> <Chip><DiNodejsSmall/> Express</Chip> <Chip><DiMysql/> MySQL</Chip> <Chip><DiMongodb/> MongoDB</Chip> <Chip><DiPostgresql/> PostgreSQL</Chip></div>
+  </div>
 </div>
 </div>
 

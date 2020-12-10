5b29ac3b5fab838e6f14e31704b8d1fbc512c5c8
Daniel DePaoli <dannydepaoli0@gmail.com>
2020-12-09 21:12:41 -0800

    Resume timeline styling

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { GrEmptyCircle } from "react-icons/gr";
 
 var Resume = (props) => (
 
@@ -7,8 +7,13 @@ var Resume = (props) => (
 
 <div style={{height: '20em', position: 'relative'}} className="AboutMe-intro" id="onload-Primary">
 
-  <h1 className="AboutMe-ThanksForStoppingBy">Resume</h1>
-<p>Code and Stuff</p>
+<h1 className="AboutMe-ThanksForStoppingBy">Resume</h1>
+<div className="Resume-CareerTimeline">
+  <div className = "Resume-Trail"/>
+  <div className = "Resume-Icon"/>
+
+{/* <GrEmptyCircle className = "Resume-Icon"/> */}
+</div>
 </div>
 
 

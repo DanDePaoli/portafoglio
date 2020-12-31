4a98cd248e5a5def3fe5bbb109f4b5bed6ae50c3
Daniel DePaoli <dannydepaoli0@gmail.com>
2020-12-30 16:46:04 -0800

    Simplified carousel schema

--- db/seed-carousel.js
+++ db/seed-carousel.js
@@ -11,33 +11,18 @@ const avgRating = [4.53, 3.75, 4.78, 3.25, 4.93, 4.23, 4.11, 4.42];
 const numratings = [51, 130, 255, 42, 85, 352];
 
 const image = [
-'https://a0.muscache.com/im/pictures/15273358/d7329e9a_original.jpg',
-'https://a0.muscache.com/im/pictures/0d4ee8d6-f92e-44f9-bf1b-6380ec7ddafd.jpg',
-'https://a0.muscache.com/im/pictures/e2c3675f-d59d-429d-9540-39ff93dcf8b5.jpg',
-'https://a0.muscache.com/im/pictures/78253bb2-1bf9-4dc4-82f4-51fead9429a8.jpg',
-'https://a0.muscache.com/im/pictures/b56bf6b4-f106-4c6b-8367-a02b12d862be.jpg',
-'https://a0.muscache.com/im/pictures/905f893e-71c1-4e3a-8237-444fa4d83e64.jpg',
-'https://a0.muscache.com/im/pictures/7bf4e61b-85de-46d5-a138-405722417737.jpg',
-'https://a0.muscache.com/im/pictures/dc09ee21-27e9-4dcd-9b59-7ba7ade0563f.jpg',
-'https://a0.muscache.com/im/pictures/5fc8b312-b262-4473-8771-69dd27909881.jpg',
-'https://a0.muscache.com/im/pictures/591ba424-1be3-4333-943f-dd7b3c5ec820.jpg',
-'https://a0.muscache.com/im/pictures/7cc3c855-f90e-4d0f-9b13-3b5c2a3c4bad.jpg',
-'https://a0.muscache.com/im/pictures/29099674/7e3509fe_original.jpg',
-'https://a0.muscache.com/im/pictures/f2a6d022-23fc-4701-b703-806870cd9ed7.jpg',
-'https://a0.muscache.com/im/pictures/9a6d5c2d-2bdd-49d6-9a2d-245762f27a66.jpg',
-'https://a0.muscache.com/4ea/air/v2/pictures/0040732e-2111-4367-b9c4-ccbb0f26c4b1.jpg',
-'https://a0.muscache.com/4ea/air/v2/pictures/ef72f44a-52a4-4d54-abdd-5043243c9fd5.jpg',
-'https://a0.muscache.com/4ea/air/v2/pictures/be8e851b-1dce-44dd-9404-fe4efc969ccc.jpg',
-'https://a0.muscache.com/4ea/air/v2/pictures/b143ebed-5987-4af7-85f4-b2af740978c0.jpg',
-'https://a0.muscache.com/4ea/air/v2/pictures/63d7a3af-2bbc-4a60-a017-69c707f44e5d.jpg',
-'https://a0.muscache.com/4ea/air/v2/pictures/e3bbe73a-28c8-433e-9321-a6037451833d.jpg',
-'https://a0.muscache.com/4ea/air/v2/pictures/a8394572-a7a7-4d5a-9fde-7e09e6e6172e.jpg',
-'https://a0.muscache.com/4ea/air/v2/pictures/763979f5-b788-4eb0-86cf-b5b8e948e2f4.jpg',
-'https://a0.muscache.com/4ea/air/v2/pictures/06045493-31f0-4583-9158-a9f556e708fd.jpg',
-'https://a0.muscache.com/4ea/air/v2/pictures/e5e58e85-6d76-46df-9258-291e8cc725b8.jpg',
-'https://a0.muscache.com/im/pictures/10675d61-25f6-4394-be7a-f81b3254508d.jpg',
-'https://a0.muscache.com/im/pictures/58f5f0ed-7f42-42d6-8527-1f6cc992b1ae.jpg',
-'https://a0.muscache.com/4ea/air/v2/pictures/ba0f02ff-1dfa-4d76-b920-a9b81f5846a0.jpg'];
+'https://ddpgithubprojects.s3-us-west-2.amazonaws.com/airbnbs/airbnb1.jpg',
+'https://ddpgithubprojects.s3-us-west-2.amazonaws.com/airbnbs/airbnb2.jpg',
+'https://ddpgithubprojects.s3-us-west-2.amazonaws.com/airbnbs/airbnb3.jpg',
+'https://ddpgithubprojects.s3-us-west-2.amazonaws.com/airbnbs/airbnb4.jpg',
+'https://ddpgithubprojects.s3-us-west-2.amazonaws.com/airbnbs/airbnb5.jpg',
+'https://ddpgithubprojects.s3-us-west-2.amazonaws.com/airbnbs/airbnb6.jpg',
+'https://ddpgithubprojects.s3-us-west-2.amazonaws.com/airbnbs/airbnb7.jpg',
+'https://ddpgithubprojects.s3-us-west-2.amazonaws.com/airbnbs/airbnb8.jpg',
+'https://ddpgithubprojects.s3-us-west-2.amazonaws.com/airbnbs/airbnb9.jpg',
+'https://ddpgithubprojects.s3-us-west-2.amazonaws.com/airbnbs/airbnb10.jpg',
+'https://ddpgithubprojects.s3-us-west-2.amazonaws.com/airbnbs/airbnb11.jpg',
+'https://ddpgithubprojects.s3-us-west-2.amazonaws.com/airbnbs/airbnb12.jpg'];
 
 const rate = [425, 623, 89, 123, 342, 122, 456, 324, 144, 126];
 
@@ -94,7 +79,7 @@ writeNTimes = (writer, min, max, callback) => {
         dataString += `"description":"${description[min % 5]}",`;
         dataString += `"avgRating":${avgRating[min % 8]},`;
         dataString += `"numratings":${numratings[min % 6]},`;
-        dataString += `"image":"${image[min % 30]}",`;
+        dataString += `"image":"${image[min % 12]}",`;
         dataString += `"rate":${rate[min % 10]},`;
         dataString += `"wasLiked":${wasLiked[min % 2]},`;
         dataString += `"postedDate":"${postedDate[min % 4]}",`;
@@ -121,10 +106,10 @@ writeNTimes = (writer, min, max, callback) => {
 }
 
 
-const writeStream = fs.createWriteStream('./seeddata/mongoseeddata.json')
+const writeStream = fs.createWriteStream('./db/mongoseeddata.json')
 
 // const line1 = 'id,name,description,avgRating,numratings,image,rate,wasLiked,postedDate,longitude,latitude,category,superHost,relatedPlaces\n';
 // writeStream.write(line1);
-writeNTimes(writeStream, 1, 100, ()=>{
+writeNTimes(writeStream, 1, 20, ()=>{
   console.log('written!')
 })
